6f5d123ad0e5dd06b8b8bd2ca299b02e386704b4
rafaErion <rafagtsouza.jr@gmail.com>
2025-02-25 15:06:55 -0300

    ajustes js value

--- main.js
+++ main.js
@@ -13,24 +13,25 @@ form.addEventListener('submit', function(e) {
 });
 
 function adiconarContato(){
-    const nome = document.getElementById('nome-contato').value.trim();
-    const telefone = document.getElementById('telefone').value.trim();
-    if(arrayNome.includes(nome) || arrayTelefone.includes(telefone)){
+    const nome = document.getElementById('nome-contato');
+    const telefone = document.getElementById('telefone');
+    if(arrayNome.includes(nome.value.trim()) || arrayTelefone.includes(telefone.value.trim())){
         alert('Já possiu esse contato!')
     }else{
-        arrayNome.push(nome);
-        arrayTelefone.push(telefone)
+        arrayNome.push(nome.value.trim());
+        arrayTelefone.push(telefone.value.trim())
         
         let linha = '<tr>';
-        linha += `<td>${nome}</td>`;
-        linha += `<td>${telefone}</td>`;
+        linha += `<td>${nome.value}</td>`;
+        linha += `<td>${telefone.value}</td>`;
         linha += '</tr>';
         
         listaDeContatos += linha;
         
         document.querySelector('tbody').innerHTML = listaDeContatos;
     }
-    
+    nome.value = '';
+    telefone.value = '';
 }
 
 
