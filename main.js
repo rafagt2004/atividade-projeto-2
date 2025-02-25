6c43c530f5b2cd92816a2badfd17e43a4ca03db0
rafaErion <rafagtsouza.jr@gmail.com>
2025-02-25 14:48:23 -0300

    add functions

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 const form = document.getElementById('form-cadastro');
+const arrayNome = [];
+const arrayTelefone = [];
+
+let listaDeContatos = '';   
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-
-    const nome = document.getElementById('nome-contato').value;
-    const telefone = document.getElementById('telefone').value;
-
-    let linha = '<tr>';
-    linha += `<td>${nome}</td>`;
-    linha += `<td>${telefone}</td>`;
-    linha += '</tr>';
-    document.querySelector('tbody').innerHTML += linha;
-
+    
+    adiconarContato();
+    totalContatos();
+    
 });
+
+function adiconarContato(){
+    const nome = document.getElementById('nome-contato').value.trim();
+    const telefone = document.getElementById('telefone').value.trim();
+    if(arrayNome.includes(nome) || arrayTelefone.includes(telefone)){
+        alert('Já possiu esse contato!')
+    }else{
+        arrayNome.push(nome);
+        arrayTelefone.push(telefone)
+        
+        let linha = '<tr>';
+        linha += `<td>${nome}</td>`;
+        linha += `<td>${telefone}</td>`;
+        linha += '</tr>';
+        
+        listaDeContatos += linha;
+        
+        document.querySelector('tbody').innerHTML = listaDeContatos;
+    }
+    
+}
+
+
+function totalContatos(){
+    const quantidadeContatos = document.querySelectorAll('tbody tr').length;
+    document.querySelector('#total-contatos').innerHTML = quantidadeContatos;
+}
